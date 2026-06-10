e29c6672764d6473eaa1da1c43d9f890152b64b3
musishere <mustafabukhari333@gmail.com>
2026-06-10 18:08:48 +0500

    Implement GET method for product retrieval in productHandler
    - Added handling for HTTP GET requests to retrieve products.
    - Introduced a new getProducts function to manage product data retrieval and response formatting.
    - Responds with 405 Method Not Allowed for unsupported HTTP methods.

--- product-api/handlers/productHandler.go
+++ product-api/handlers/productHandler.go
@@ -16,10 +16,18 @@ func NewProduct(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 	err := lp.Tojson(rw)
 	if err != nil {
 		http.Error(rw, "internal servert error", http.StatusInternalServerError)
 	}
-
 }
