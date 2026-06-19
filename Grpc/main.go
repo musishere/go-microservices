95a65b409f4e65c54c30067477d5819f16be5cb7
musishere <mustafabukhari333@gmail.com>
2026-06-19 17:09:30 +0500

    Implement currency service with gRPC and update Makefile
    - Added Currency service implementation with GetRate method in server/currency.go.
    - Enhanced main.go to initialize gRPC server and register Currency service.
    - Defined RateRequest and RateResponse messages in currency.proto for request and response handling.
    - Updated Makefile to include a new target for generating protocol buffers.
    - Added generated files for gRPC client and server stubs in protos directory.

--- Grpc/main.go
+++ Grpc/main.go
@@ -1,3 +1,39 @@
 package main
 
-func main() {}
+import (
+	"net"
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+	currency "github.com/musishere/grpc/protos"
+	"github.com/musishere/grpc/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	// Register a Logger
+	log := hclog.Default()
+
+	// Invoke a Service
+	cs := server.NewCurrency(log)
+
+	// Initialize a grpc server
+	gs := grpc.NewServer()
+
+	// Register grpc and service server
+	currency.RegisterCurrencyServer(gs, cs)
+
+	// Expose the grpc server
+	reflection.Register(gs)
+
+	// Register a listner
+	l, err := net.Listen("tcp", ":8010")
+	if err != nil {
+		log.Error("Error on making listner", err)
+		os.Exit(1)
+	}
+
+	// Start the server
+	gs.Serve(l)
+}
--- Grpc/server/currency.go
+++ Grpc/server/currency.go
@@ -1,1 +1,26 @@
 package server
+
+import (
+	"context"
+
+	"github.com/hashicorp/go-hclog"
+	currency "github.com/musishere/grpc/protos"
+)
+
+type Currency struct {
+	currency.UnimplementedCurrencyServer
+	log hclog.Logger
+}
+
+func NewCurrency(l hclog.Logger) *Currency {
+	return &Currency{
+		log: l,
+	}
+}
+
+func (c *Currency) GetRate(ctx context.Context, in *currency.RateRequest) (*currency.RateResponse, error) {
+	c.log.Info("Handle GetRate", "base", in.GetBase(), "destination", in.Destination)
+	return &currency.RateResponse{
+		Rate: 30.5,
+	}, nil
+}
